7deb34be15e880cfdefc81c8581f705d34a56adf
TungVT <vanthanhtung1562013@gmail.com>
2022-06-04 16:41:42 +0700

    RE example

---
+++ src/main/java/com/springhow/examples/drools/domain/ElectricBill.java
@@ -0,0 +1,40 @@
+package com.springhow.examples.drools.domain;
+
+public class ElectricBill {
+    private Integer id;
+    private String area;
+    private Integer totalNumber;
+    private Double totalPrice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Integer getTotalNumber() {
+        return totalNumber;
+    }
+
+    public void setTotalNumber(Integer totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
